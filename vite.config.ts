babf4b82e5176ed53995d0bbea2a7729c265631c
Xiaofan_Mok <Xiaofan_Mok@singaporeair.com.sg>
2023-12-11 17:20:59 +0800

    Set up favicon and remove unused files

--- vite.config.ts
+++ vite.config.ts
@@ -13,17 +13,17 @@ const pwaOptions: Partial<VitePWAOptions> = {
     theme_color: '#ffffff',
     icons: [
       {
-        src: 'pwa-192x192.png', // <== don't add slash, for testing
+        src: 'logo192.png', // <== don't add slash, for testing
         sizes: '192x192',
         type: 'image/png',
       },
       {
-        src: '/pwa-512x512.png', // <== don't remove slash, for testing
+        src: '/logo512.png', // <== don't remove slash, for testing
         sizes: '512x512',
         type: 'image/png',
       },
       {
-        src: 'pwa-512x512.png', // <== don't add slash, for testing
+        src: 'logo512.png', // <== don't add slash, for testing
         sizes: '512x512',
         type: 'image/png',
         purpose: 'any maskable',
