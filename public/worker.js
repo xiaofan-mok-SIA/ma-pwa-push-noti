bbaac8a937240187686d2f637b69a621722460ac
Xiaofan_Mok <Xiaofan_Mok@singaporeair.com.sg>
2024-03-01 13:59:50 +0800

    fix json to text

--- public/worker.js
+++ public/worker.js
@@ -3,31 +3,27 @@
 console.log("Service Worker Loaded...");
 
 self.addEventListener("activate", async (e) => {
-    console.log("Service Worker: Activated");    
+	console.log("Service Worker: Activated");
 });
 
 self.addEventListener("push", (event) => {
-    console.log('Push Received...')
-    if (!(self.Notification && self.Notification.permission === "granted")) {
-        return;
-    }
+	console.log("Push Received...");
+	if (!(self.Notification && self.Notification.permission === "granted")) {
+		return;
+	}
 
-    const data = event.data?.json() ?? {};
-    const title = data.title || "Something Has Happened";
-    const message =
-        data.message || "Here's something you might want to check out.";
-    const icon = "images/new-notification.png";
+	const data = event.data?.text() ?? {};
+	const title = data.title || "Something Has Happened";
+	const message = data.message || "Here's something you might want to check out.";
+	const icon = "images/new-notification.png";
 
-    const notification = new self.Notification(title, {
-        body: message,
-        tag: "simple-push-demo-notification",
-        icon,
-    });
+	const notification = new self.Notification(title, {
+		body: message,
+		tag: "simple-push-demo-notification",
+		icon
+	});
 
-    notification.addEventListener("click", () => {
-        clients.openWindow(
-            "https://example.blog.com/2015/03/04/something-new.html",
-        );
-    });
+	notification.addEventListener("click", () => {
+		clients.openWindow("https://example.blog.com/2015/03/04/something-new.html");
+	});
 });
-
